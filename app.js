d43e0ff5bbacfde79bbe769e34db1a6b4db5bd2d
LuissFrank <104394104+LuissFrank@users.noreply.github.com>
2023-11-02 01:12:31 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// Animación para mostrar/ocultar el botón de WhatsApp
+const whatsappButton = document.getElementById('whatsapp-button');
+
+whatsappButton.addEventListener('click', () => {
+   window.location.href = 'https://walink.co/5e4c2a';
+});
+
+// Agrega una animación de entrada
+whatsappButton.style.transform = 'scale(0)';
+setTimeout(() => {
+  whatsappButton.style.transform = 'scale(1)';
+}, 1000);
